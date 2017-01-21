61a30ba88b30ce2c823266b9f7a4c3dcab7d8c8b
Jon Newton <jon.newton@gmail.com>
2017-01-21 15:20:08 -0700

    initial spec for ADT definition

--- src/adt_spec/core.clj
+++ src/adt_spec/core.clj
@@ -1,6 +1,98 @@
-(ns adt-spec.core)
+(ns adt-spec.core
+  (:require [clojure.spec :as s]
+            [clojure.core.match :refer [match]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn is
+  [v]
+  #(= % v))
+
+(defn tree-of
+  "Returns a spec for a tree of type t? where t? is a spec."
+  [t?]
+  (s/or
+    ::empty (s/tuple (is :empty))
+    ::leaf (s/tuple (is :leaf) t?)
+    ::node (s/tuple (is :node) ::tree ::tree)))
+
+(defn maybe-of
+  [t?]
+  (s/or
+    ::none (s/tuple (is :none))
+    ::some (s/tuple (is :some) t?)))
+
+(defn result-of
+  [ok? err?]
+  (s/or
+    ::error (s/tuple (is :error) err?)
+    ::ok (s/tuple (is :ok) ok?)))
+
+;(sum ::tree
+;     (prod ::empty)
+;     (prod ::leaf 'a)
+;     (prod ::node ::tree ::tree))
+
+(s/def ::field
+  (s/or
+    ::generic symbol?
+    ::predicate fn?
+    ;;::spec #(s/spec? %)
+    ::spec keyword?
+    ))
+
+(s/def ::product
+  (s/or
+    :empty-prod keyword?
+    :prod (s/cat
+            ::variant keyword?
+            ::fields (s/+ ::field))))
+
+(s/def ::adt
+  (s/cat ::type keyword?
+         ::generics (s/* symbol?)
+         ::products (s/* ::product)))
+
+(defn adt
+  "Returns a clojure spec for the ADT"
+  [definition & generics]
+  )
+
+(def tree
+  [::tree 'a
+   :empty
+   [:leaf 'a]
+   [:node ::tree ::tree]])
+
+(def point
+  [::point
+   [:point double? double? double?]
+   [:point {:x double? :y double? :z double?}]])
+
+(def list
+  (partial adt [::list 'a
+                :nil
+                [:cons 'a ::list]]))
+
+(list int?)                                                 ;; -> spec of List<Long>
+
+(def maybe
+  [::maybe 'a
+   :none
+   [:some 'a]])
+
+(def result
+  [::result 'o 'e
+   [:ok 'o]
+   [:err 'e]])
+
+(def color
+  [::color
+   :red
+   :blue
+   :green])
+
+(def temperature
+  (adt [::temp
+        [:kelvin (s/and double? pos?)]
+        [:celcius (s/and double? #(> 273.15))]
+        [:farenheit (s/and double? #(> 459.67))]]))
+
